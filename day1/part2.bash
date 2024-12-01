a0efc26bead5c9587f98c525ed64ac686ec7a352
Sina Siadat <siadat@gmail.com>
2024-12-02 00:40:46 +0100

    day1, part2

---
+++ day1/part2.bash
@@ -0,0 +1,10 @@
+cat day1/input | awk '{print $1}' > /tmp/col1
+cat day1/input | awk '{print $2}' > /tmp/col2
+
+cat /tmp/col1 | while read -r line; do
+	freq="$(cat /tmp/col2 | grep -w "$line" | uniq -c | col1)"
+	if [ -z "$freq" ]; then
+		continue
+	fi
+	echo "$line * $freq" | bc
+done | paste -sd+ | bc

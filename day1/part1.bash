76243f4e1716242408e3b7a74f520695b372231f
Sina Siadat <siadat@gmail.com>
2024-12-02 01:53:20 +0100

    TIL passing multiple args to read

--- day1/part1.bash
+++ day1/part1.bash
@@ -1,6 +1,6 @@
 cat day1/input | awk '{print $1}' | sort -n > /tmp/sorted1
 cat day1/input | awk '{print $2}' | sort -n > /tmp/sorted2
 
-paste -d- /tmp/sorted1 /tmp/sorted2 | while read -r line; do
-	echo "$line" | bc | perl -pe 's/^-//'
+paste /tmp/sorted1 /tmp/sorted2 | while read -r left right; do
+	echo "$left - $right" | bc | perl -pe 's/^-//'
 done | paste -sd+ | bc

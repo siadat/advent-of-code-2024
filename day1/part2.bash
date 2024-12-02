43067336d8e527e00e5930f24caf686fbc5a3570
Sina Siadat <siadat@gmail.com>
2024-12-02 03:31:26 +0100

    use simpler col1 and col2 instead of awk

--- day1/part2.bash
+++ day1/part2.bash
@@ -1,5 +1,5 @@
-cat day1/input | awk '{print $1}' > /tmp/col1
-cat day1/input | awk '{print $2}' > /tmp/col2
+cat day1/input | col1 > /tmp/col1
+cat day1/input | col2 > /tmp/col2
 
 cat /tmp/col1 | while read -r line; do
 	freq="$(cat /tmp/col2 | grep -w "$line" | uniq -c | col1)"

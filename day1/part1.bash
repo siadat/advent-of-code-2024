54a44ade13d993dc74e123937b68a99f57bc9cf6
Sina Siadat <siadat@gmail.com>
2024-12-02 15:18:37 +0100

    sed is faster than perl -pe (0m1.448s vs 0m2.679s)

--- day1/part1.bash
+++ day1/part1.bash
@@ -2,5 +2,5 @@ cat day1/input | col1 | sort -n > /tmp/sorted1
 cat day1/input | col2 | sort -n > /tmp/sorted2
 
 paste /tmp/sorted1 /tmp/sorted2 | while read -r left right; do
-	echo "$left - $right" | bc | perl -pe 's/^-//'
+	echo "$left - $right" | bc | sed 's/^-//'
 done | paste -sd+ | bc

e03b721ceb489797e153736a7ed25e5bc5128cc6
Sina Siadat <siadat@gmail.com>
2024-12-02 01:11:18 +0100

    I want to run everything from the root

--- day1/part1.bash
+++ day1/part1.bash
@@ -1,5 +1,5 @@
-cat input | awk '{print $1}' | sort -n > /tmp/sorted1
-cat input | awk '{print $2}' | sort -n > /tmp/sorted2
+cat day1/input | awk '{print $1}' | sort -n > /tmp/sorted1
+cat day1/input | awk '{print $2}' | sort -n > /tmp/sorted2
 
 paste -d- /tmp/sorted1 /tmp/sorted2 | while read -r line; do
 	echo "$line" | bc | perl -pe 's/^-//'

5fe0efa0ac2c618f63bce9bb3414729192a8e9f5
Sina Siadat <siadat@gmail.com>
2024-12-03 15:46:23 +0100

    make part1 more like part2

--- day2/part1.bash
+++ day2/part1.bash
@@ -1,8 +1,5 @@
 cat day2/input | while read -r report; do
-	(
-		echo "1 3"
-		echo "-3 -1"
-	) | while read -r min max; do
+	while read -r min max; do
 		transform="
 			s/([0-9]+)/\1 \1/g;
 			s/(^[0-9]+ | [0-9]+$)//g;
@@ -11,8 +8,11 @@ cat day2/input | while read -r report; do
 		"
 		is_good="$(echo "$report" | sed -r "$transform" | bc)"
 		if [ "$is_good" -eq 1 ]; then
-			echo "good"
+			echo "good: $report"
 			break
 		fi
-	done
-done | wc -l
+	done < <(
+		echo "1 3"
+		echo "-3 -1"
+	)
+done | nl

5df38e4e3f99514e605217c462ff22a16e5f1138
Sina Siadat <siadat@gmail.com>
2024-12-04 20:06:33 +0100

    disable buffering for sed perl grep

--- day3/part2.bash
+++ day3/part2.bash
@@ -1,6 +1,8 @@
 cat day3/input \
-	| grep -Po -e "mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)" \
-	| sed "/don't()/,/do()/d" \
-	| grep -vw -P "do\(\)|don't\(\)" \
-	| perl -pe 's#mul\((\d{1,3}),(\d{1,3})\)#\1 * \2#' \
-	| bc | paste -sd+ | bc
+	| grep --line-buffered -Po -e "mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)" \
+	| sed -u "/don't()/,/do()/d" \
+	| grep --line-buffered -vw -P "do\(\)|don't\(\)" \
+	| perl -pe 'BEGIN { $| = 1 } s#mul\((\d{1,3}),(\d{1,3})\)#\1 * \2#' \
+	| bc \
+	| paste -sd+ \
+	| bc

b58482333cd5143d86197fd26c90afcf365adba5
Sina Siadat <siadat@gmail.com>
2024-12-06 02:12:51 +0100

    stupid simple rewrite
    because apparently all sequences can only be sorted in exactly one way,
    ie for a sequence with N items, there's always an item after which (N-1)
    items must be placed, an item after which (N-2)... until 0. And
    therefore you know exactly which one to place first, second, etc.

--- day5/part2.bash
+++ day5/part2.bash
@@ -10,41 +10,22 @@ done
 
 # Check and correct the sequences
 cat day5/input | grep , | nl | while read -r seqIdx sequence; do
-	is_corrected=
-	while true; do
-		dir=$root/sequence$seqIdx
-		echo "Checking sequence$seqIdx $sequence..." > /dev/stderr
-		rm -rf $root/sequence$seqIdx
-		while read -r pageIdx page; do
-			dir=$dir/$page
-			mkdir -p $dir
-			ln -s $root/forward/$page $dir/nexts
-			echo $pageIdx > $dir/pageIdx
-		done < <(echo "$sequence" | grep -Po '\d+' | nl)
+	echo "Checking sequence$seqIdx $sequence..." > /dev/stderr
+	sequence_regex="$(echo "$sequence" | sed 's/,/|/g')"
 
-		dir=$root/sequence$seqIdx
-		while read -r pageIdx page; do
-			dir=$dir/$page
-			matches="$(grep -w $page -R $dir --exclude="pageIdx" -l)"
-			while read -r match; do
-				if [ -z "$match" ]; then continue; fi
-				badIdx=$(cat $(dirname $(dirname $match))/pageIdx)
-				IFS=',' read -r -a sequence_array <<< "$sequence"
-				tmp=${sequence_array[$((pageIdx-1))]}
-				sequence_array[$((pageIdx-1))]=${sequence_array[$((badIdx-1))]}
-				sequence_array[$((badIdx-1))]=$tmp
-				sequence=$(IFS=,; echo "${sequence_array[*]}")
-				is_corrected=true
-				continue 3
-			done < <(echo "$matches" | tail -1)
-		done < <(echo "$sequence" | grep -Po '\d+' | nl)
-		break
-	done
+	# Sort items in sequence by the number of dependencies in the sequence
+	echo "$sequence" | grep -Po '\d+' | while read -r page; do
+		dependency_count="$(grep -P "$sequence_regex" -R $root/forward/$page/direct | wc -l)"
+		echo "$dependency_count $page"
+	done | sort -k1 -nr | col2 | paste -sd, > $root/sorted_sequence 
 
-	if [ -z "$is_corrected" ]; then
+	if [ "$sequence" == "$(cat $root/sorted_sequence)" ]; then
+		# already good
 		continue
 	fi
-	echo "Good: sequence$seqIdx $sequence" > /dev/stderr
+
+	sequence="$(cat $root/sorted_sequence)"
+	echo "Corrected: $sequence" > /dev/stderr
 	length="$(echo "$sequence" | grep -Po '\d+' | wc -l)"
 	middle_idx=$"$(( (length+1) / 2))"
 	echo "$sequence" | grep -Po '\d+' | sed -n "$middle_idx"p

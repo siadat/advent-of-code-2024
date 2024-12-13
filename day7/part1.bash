7a09ac8ae7269a658e20b745589032163ff6a839
Sina Siadat <siadat@gmail.com>
2024-12-13 16:35:23 +0100

    make day7/part1 more readable

--- day7/part1.bash
+++ day7/part1.bash
@@ -1,1 +1,9 @@
-cat day7input | while IFS=: read res line; do eval "echo '($(yes '(' | head -n $(echo $line | wc -w) | paste -sd '')'$(echo $line|sed -e 's# #\{+,\\*\}#g')==$res" | sed 's# # \|\| #g' | while read eq; do echo "(($eq)*$res"; done | sed -r -e "s#[0-9]+#\0\)#g" | tee /dev/stderr| bc; done |tee /dev/stderr| paste -sd+|bc
+cat day7/input | while IFS=: read res line; do
+	num_count=$(echo $line | wc -w)
+	open_parens="($(yes '(' | head -n $num_count | paste -sd '')"
+	nums="$(echo $line | sed -e 's/ /\{+,\\*\}/g')"
+	permutations="$(eval "echo '$open_parens'$nums==$res" | sed 's/ / \|\| /g')"
+	echo "$permutations" | while read eq; do
+		echo "(($eq)*$res"
+	done | sed -r -e "s/[0-9]+/\0\)/g" | tee /dev/stderr | bc
+done | tee /dev/stderr | paste -sd+ | bc
